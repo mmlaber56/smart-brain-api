7a55e34a64738d3e8f8dce5b28d551ab50ba7a27
MeLaber <mmlaber@gorinsystems.com>
2021-12-15 22:33:09 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'dev',
+    password : 'fourwordslowercase',
+    database : 'smart-brain'
+  }
+});
+
+
+// db.select('*').from('users').then(data => {
+// 	console.log(data);
+// });
+
+const app = express();
+
+
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) =>  {
+	res.json('This is no longer used');
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req,res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req,res, bcrypt, db)})
+
+app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,db)})
+
+app.put('/image',(req, res) => {image.handleImage(req,res,db)})
+
+app.post('/imageurl',(req, res) => {image.handleApiCall(req,res,)})
+
+app.listen(3001, ()=> {
+	console.log('app is running on port 3001')
+})
+
+
+
+/*
+/ --> res = this is working
+/signin  --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
