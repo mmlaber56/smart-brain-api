4e79768b2ae55ec61caecc7b8cde5ce857d7c474
MeLaber <mmlaber@gorinsystems.com>
2021-12-18 21:59:25 -0500

    Reset pg host for Heroku

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-flexible-52966',
-    user : 'dev',
-    password : 'fourwordslowercase',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
